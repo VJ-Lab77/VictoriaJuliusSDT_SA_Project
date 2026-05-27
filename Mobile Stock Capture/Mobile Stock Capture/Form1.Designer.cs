c1ddeec72222bf5fe1ff744d48d22c30f4a7fff0
Victoria <20241168@ctucareer.co.za>
2026-05-27 11:24:13 +0200

    Mobile Stock Capture Windows Forms application

---
+++ Mobile Stock Capture/Mobile Stock Capture/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Mobile_Stock_Capture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.lblMobileCode = new System.Windows.Forms.Label();
+            this.lblMake = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(246, 40);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 16);
+            this.lblOutput.TabIndex = 0;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(302, 101);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(100, 22);
+            this.txtCode.TabIndex = 2;
+            // 
+            // lblMobileCode
+            // 
+            this.lblMobileCode.AutoSize = true;
+            this.lblMobileCode.Location = new System.Drawing.Point(181, 107);
+            this.lblMobileCode.Name = "lblMobileCode";
+            this.lblMobileCode.Size = new System.Drawing.Size(81, 16);
+            this.lblMobileCode.TabIndex = 3;
+            this.lblMobileCode.Text = "MobileCode";
+            // 
+            // lblMake
+            // 
+            this.lblMake.AutoSize = true;
+            this.lblMake.Location = new System.Drawing.Point(184, 151);
+            this.lblMake.Name = "lblMake";
+            this.lblMake.Size = new System.Drawing.Size(41, 16);
+            this.lblMake.TabIndex = 4;
+            this.lblMake.Text = "Make";
+            this.lblMake.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(184, 192);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(55, 16);
+            this.lblQuantity.TabIndex = 5;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(302, 151);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(100, 22);
+            this.txtMake.TabIndex = 6;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(302, 192);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 22);
+            this.txtQuantity.TabIndex = 7;
+            this.txtQuantity.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(129, 287);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(270, 287);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(393, 287);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 10;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtMake);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblMake);
+            this.Controls.Add(this.lblMobileCode);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.lblOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label lblMobileCode;
+        private System.Windows.Forms.Label lblMake;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFind;
+    }
+}
+
---
+++ Mobile Stock Capture/Mobile Stock Capture/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mobile_Stock_Capture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Record Added");
+            Console.Write("Mobile Code: " + txtCode.Text);
+            Console.WriteLine("Make: " + txtMake.Text);
+            Console.WriteLine("Quantity: " + txtQuantity.Text);
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Record Deleted");
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Record Found");
+        }
+
+        private void lblOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
